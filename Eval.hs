3b6a8a96cbd23993720e7f223bb5b747e7363e63
Daniel Choi <dhchoi@gmail.com>
2016-04-23 17:32:55 -0400

    use Reader monad, sketching pattern matches for eval

--- Eval.hs
+++ Eval.hs
@@ -3,6 +3,32 @@ module Eval where
 import Expression
 import Control.Applicative
 import qualified Data.Map as M
+import Data.Text (Text)
+import Data.Functor.Identity (Identity )
+import Control.Monad.Reader
 
+type Reader' = Reader Context 
 
+data Context = Context {
+      fields :: [Text] -- the DSV fields
+    , trueStrings :: [Text]
+    , falseStrings :: [Text]
+    , nullStrings :: [Text]
+    } deriving Show
+
+exprEvalToString :: Expr -> Reader' String
+exprEvalToString e = undefined
+
+evalToBool :: Expr -> Reader' Bool
+evalToBool e =
+    let val = exprEval expr context
+    in valueToBool val
+
+-- handle all the cases
+exprEval :: Expr -> Context -> 
+exprEval (FieldNum n) ==
+exprEval (And x y) =
+exprEval (Or x y) =
+exprEval (StringChoice map) =
+exprEval (Compare op x y) =
 
