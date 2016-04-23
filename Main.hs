f24225797019682452b82046dba6505f3e20fdb3
Daniel Choi <dhchoi@gmail.com>
2016-04-23 16:58:09 -0400

    more expr parsing code

--- Main.hs
+++ Main.hs
@@ -4,10 +4,12 @@ import qualified Data.Text.Lazy.IO as T
 import qualified Data.Text as T
 import Data.Text (Text)
 import Data.Maybe
+import qualified Data.Map as M
 import Options.Applicative ((<>))
 import qualified Options.Applicative as O
 import Control.Applicative
-import Data.Attoparsec.Text 
+import Text.Parsec hiding (many, (<|>))
+import Data.Functor.Identity (Identity)
 
 data Options = Options {
       delimiter :: Text
@@ -35,3 +37,63 @@ main = do
     Options{..} <- O.execParser opts
     undefined
      
+data Expr = FieldNum Int
+          | And Expr Expr
+          | Or Expr Expr
+          | Compare String Expr Expr
+          | StringChoice (M.Map String Expr)
+          | LiteralExpr Literal
+          deriving (Eq, Show)
+
+data Literal = LitString String | LitNumber Int | LitBool Bool
+    deriving (Eq, Show)
+
+data TextChunk = PassThrough String | Interpolation String 
+    deriving (Show, Eq)
+
+data ComparableValue = ComparableNumberValue Int
+                     | ComparableStringValue String
+                     | ComparableBoolValue Bool
+                     | ComparableNullValue 
+    deriving (Ord, Eq, Show)
+
+type ExprParser = ParsecT String () Identity 
+
+expr = do
+    stringChoice <|> (do
+      expr1 <- exprTerm
+      try (do symbol "&&"; expr2 <- expr; return $ And expr1 expr2) 
+       <|> try (do symbol "||"; expr2 <- expr; return $ Or expr1 expr2) 
+       <|> try (do op <- comparisonOp; expr2 <- expr; return $ Compare op expr1 expr2) 
+       <|> return expr1)
+
+stringChoice = do
+    char '{' >> spaces
+    pairs :: [(String, Expr)] <- sepBy1 ((,) <$> varName <*> (char ':' >> spaces >> expr <* spaces)) (char ',' >> spaces)
+    spaces >> char '}' >> spaces
+    return $ StringChoice $ M.fromList pairs
+
+symbol :: String -> ExprParser String
+symbol s = spaces *> string s <* spaces
+
+comparisonOp = choice $ map (try . symbol) [">=", "<=", "!=", ">", "<", "=="]
+
+exprTerm :: ExprParser Expr
+exprTerm = (char '(' *> expr <* char ')') <|> literalExpr <|> fieldNum
+
+fieldNum :: ExprParser Expr
+fieldNum = (FieldNum . read) <$> (char '$' *> many1 digit)
+
+
+varName = many1 (alphaNum <|> char '$' <|> char '_')
+
+literalExpr = LiteralExpr <$> (litNumber <|> litString <|> litBool)
+
+litNumber = LitNumber . read <$> many1 digit  
+
+-- dumb simple implementation that does not deal with escaping
+litString = LitString <$> ((char '"' *> many (noneOf "\"") <* char '"') <|> (char '\'' *> many (noneOf "'") <* char '\''))
+
+litBool = LitBool <$> ((try (string "true") *> pure True) <|> (try (string "false") *> pure False))
+
+
