673be5c9df5fb854ac2a8e15a19900437d0d66d3
Daniel Choi <dhchoi@gmail.com>
2016-04-23 16:34:43 -0400

    start writing Main.hs

--- Main.hs
+++ Main.hs
@@ -1,5 +1,37 @@
 {-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables #-} 
 module Main where
+import qualified Data.Text.Lazy.IO as T
+import qualified Data.Text as T
+import Data.Text (Text)
+import Data.Maybe
+import Options.Applicative ((<>))
+import qualified Options.Applicative as O
+import Control.Applicative
+import Data.Attoparsec.Text 
+
+data Options = Options {
+      delimiter :: Text
+    , templateFile :: FilePath
+    }
+
+options :: O.Parser Options
+options = Options 
+    <$> (T.pack <$> 
+          (O.strOption 
+            (O.short 's' 
+            <> O.metavar "DELIMITER" 
+            <> O.value "\t"
+            <> O.help "Default: TAB")))
+    <*> O.strArgument 
+        ( O.metavar "TEMPLATE-FILE" 
+        <> O.help "Template file path")
+
+opts :: O.ParserInfo Options
+opts = O.info (O.helper <*> options) 
+          (O.fullDesc <> O.header "dsvt"
+          <> O.progDesc "DSV templating")
 
 main = do
+    Options{..} <- O.execParser opts
     undefined
+     
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# dsvt: Delimiter-Separated Value Templating
+# dsvt: 
+
+Delimiter-separated value (DSV) templating. 
+
+Takes a stream of DSV lines on STDIN, plus a HTML/XML template, and outputs one
+template unit per line.
 
 
